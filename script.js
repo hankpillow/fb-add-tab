d87999f3880830c0cfb2ecb572e42a5805947162
igor <listas@ialmeida.com>
2012-06-06 18:33:52 -0300

    url param fix

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function add_tab(){
 	var app_key = document.getElementById("app_key").value;
-	var script = "document.location.href='https://www.facebook.com/add.php?api_key="+app_key+"&tab=1'";
+	var script = "document.location.href='https://www.facebook.com/add.php?api_key="+app_key+"&pages=1'";
 	chrome.tabs.executeScript(null,{code:script});
 }
